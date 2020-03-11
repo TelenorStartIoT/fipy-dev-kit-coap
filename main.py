55a2d79a07229807c536a7d1ed8eeddf4ba1097a
tuvarb <54406563+tuvarb@users.noreply.github.com>
2020-03-11 10:36:33 +0100

    Update main.py
    small fixes

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from ujson import dumps
 from time import sleep
 
 # Imports for sensor
+import time
 import pycom
 from machine import Pin
 from dth import DTH
@@ -41,9 +42,9 @@ def run():
       # temperature = result.temperature
       # humidity = result.humidity
       # if result.is_valid():
-      # pycom.rgbled(0x001000) # green
-      # print("Temperature: %d C" % result.temperature)
-      # print("Humidity: %d %%" % result.humidity)
+      #   pycom.rgbled(0x001000) # green
+      #   print("Temperature: %d C" % result.temperature)
+      #   print("Humidity: %d %%" % result.humidity)
 
 
       # Create the data payload
